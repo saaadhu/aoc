7ebc710a550f0c59c4ae5237c7d9308163ecf00b
Senthil Kumar Selvaraj <senthil.thecoder@gmail.com>
2021-12-07 08:52:40 +0530

    Day 4

---
+++ 2021/rust/day4_1.rs
@@ -0,0 +1,111 @@
+use std::fs;
+
+
+#[derive(Debug, Clone)]
+struct BoardPosition {
+    num :u32,
+    marked: bool
+}
+
+type Board = Vec<Vec<BoardPosition>>;
+
+fn main() {
+    let mut numbers : Vec<u32> = Vec::new();
+    let mut boards : Vec<Board> = Vec::new();
+    
+    let contents = fs::read_to_string("/home/saaadhu/scratch/input4").expect("Invalid file");
+    let mut iter = contents.split("\n");
+    let numbersline = iter.next().unwrap();
+    for number in numbersline.split(",") {
+        numbers.push(number.parse().unwrap());
+    }
+    iter.next();
+
+    let mut board : Vec<Vec<BoardPosition>> = Vec::new();
+    for line in iter {
+        if line == "" {
+            boards.push(board.clone());
+            board.clear();
+            continue;
+        }
+
+        let mut row : Vec<BoardPosition> = Vec::new();
+        for part in line.split_whitespace() {
+            let num : u32 = part.parse().unwrap();
+            row.push(BoardPosition {num : num, marked: false});
+        }
+        board.push(row.clone());
+    }
+
+    boards.push(board.clone());
+
+    'outer: for num in numbers {
+        println! ("Choosing {}", num);
+
+        for i in 0..boards.len() {
+            for row in 0..board.len() {
+                for col in 0..board.len() {
+                    if boards[i][row][col].num == num {
+                        boards[i][row][col].marked = true;
+                    }
+                }
+            }
+        }
+
+        for i in 0..boards.len() {
+            for row in 0..board.len() {
+                let mut rowmarked = true;
+                for col in 0..board.len() {
+                    if !boards[i][row][col].marked {
+                        rowmarked = false;
+                        break;
+                    }
+                }
+
+                if rowmarked {
+                    println! ("Found row {} marked of board {}", row, i);
+                    let mut unmarkedsum = 0;
+
+                    for row in 0..board.len() {
+                        for col in 0..board.len() {
+                            if !boards[i][row][col].marked {
+                                unmarkedsum += boards[i][row][col].num;
+                            }
+                        }
+                    }
+                    println! ("{} * {} = {}", unmarkedsum, num, unmarkedsum * num);
+                    break 'outer;
+                }
+            }
+        }
+
+        for i in 0..boards.len() {
+            for col in 0..board.len() {
+                let mut colmarked = true;
+                for row in 0..board.len() {
+                    if !boards[i][row][col].marked {
+                        colmarked = false;
+                        break;
+                    }
+                }
+
+                if colmarked {
+                    println! ("Found col {} marked of board {}", col, i);
+
+                    let mut unmarkedsum = 0;
+                    for row in 0..board.len() {
+                        for col in 0..board.len() {
+                            if !boards[i][row][col].marked {
+                                unmarkedsum += boards[i][row][col].num;
+                            }
+                        }
+                    }
+                    println! ("{} * {} = {}", unmarkedsum, num, unmarkedsum * num);
+                    break 'outer;
+                }
+            }
+        }
+    }
+}
+
+
